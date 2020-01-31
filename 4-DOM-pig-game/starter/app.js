a6efeaf58276ce675415c964efb42dc25b1305c6
BorisAltynnik <BorisAltynnik@gmail.com>
2020-01-31 02:31:09 +0200

    current score feature update

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,18 +9,69 @@ GAME RULES:
 
 */
 
-var scores, roundScores, activePlayer, dice;
+var scores, roundScores, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0; // 0 - first player, 1 - second player
 
-dice = Math.floor(Math.random()*6 + 1); //Math.random
-console.log(dice);
 
-document.querySelector('#current-'+activePlayer).textContent = dice; 
+//console.log(dice);
 
-//magic comment
-//second magic comment
-//third magic comment
+//document.querySelector('#current-'+activePlayer).textContent = dice; 
+
+//document.querySelector('#current-'+activePlayer).innerHTML = '<em> '+ dice+ '</em>'; // to put some html code use innerHTML
+
+//var x = document.querySelector('#score-0').innerHTML;
+
+
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+
+
+
+document.querySelector('.btn-roll').addEventListener('click',function() {
+    
+    var dice = Math.floor(Math.random()*6 + 1); //Math.random
+    
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-'+dice+'.png';
+    
+    if (dice !== 1){
+        //add score
+        roundScore += dice;
+        document.querySelector('#current-'+ activePlayer).textContent = roundScore;
+    } else{
+        //Next player turn
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        
+        //document.querySelector('.player-0-panel').classList.remove('active');
+        //document.querySelector('.player-1-panel').classList.add('active');
+        
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        
+        document.querySelector('.dice').style.display = 'none';
+        
+        
+    }
+                                                     
+});
+
+
+
+
 
